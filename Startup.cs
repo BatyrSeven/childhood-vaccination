960c74c489c1af9e08ab6ff3492566649b7597ab
Batyr Seven <batyrseven@gmail.com>
2019-02-10 18:55:21 +0600

    setting up db on project start up

--- Startup.cs
+++ Startup.cs
@@ -46,6 +46,13 @@ namespace Playground
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreetingService greeting)
         {
+            // for setting up db on project start up
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+			{
+				var context = serviceScope.ServiceProvider.GetRequiredService<ChildVaccDbContext>();
+				context.Database.Migrate();
+			}
+			
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
